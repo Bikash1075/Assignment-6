f1395c30af731b77118727f7bc0de5ac21845871
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-20 14:38:47 +0530

    Add files via upload

--- Assignment1.py
+++ Assignment1.py
@@ -17,8 +17,35 @@ with open ("employee.json","w") as e:
 
 with open("employee.json","r") as e_read:
     Employee=json.load(e_read)
-print(Employee)
-
+for i, j in Employee.items():
+    print (i,"=",j)
+    
+# 2nd approach
+import json
+class Employee:
+    def __init__(self):
+        self.Employeesdata={}
+        self.e1={"emp1":{"Name" : "Bikash Ranjan Padhy" , "DOB" : "29/10/1994", "Hieght" : "5'8" , "City" : "Surada" , "State" : "Odisha" }}
+        self.e2={"emp2":{"Name" : "Firoz Ansari" , "DOB" : "22/05/1996", "Hieght" : "5'4" , "City" : "Jamshedpur" , "State" : "Jharkhand" }}
+        self.e3={"emp3":{"Name" : "Srinu Ponduru" , "DOB" : "15/07/1996", "Hieght" : "5'10" , "City" : "Beemavaram" , "State" : "Andhra Pradesh" }}
+        self.e4={"emp4":{"Name" : "Abhishek Priyadarshi" , "DOB" : "25/03/1997", "Hieght" : "5'8" , "City" : "Patna" , "State" : "Bihar" }}
+        self.e5={"emp5":{"Name" : "Jeetendra Kumar" , "DOB" : "25/08/1991", "Hieght" : "5'5" , "City" : "Budhana" , "State" : "Uttar Pradesh" }}
+        self.e6={"emp6":{"Name" : "Shivangee Mishra" , "DOB" : "10/08/1996", "Hieght" : "5'5" , "City" : "Jodhpur" , "State" : "Rajasthan" }}
+        self.e7={"emp7":{"Name" : "Kiran Pattnaik" , "DOB" : "15/9/1995", "Hieght" : "5'8" , "City" : "Pune" , "State" : "Maharastra" }}
+        self.e8={"emp8":{"Name" : "Abhishek Panda" , "DOB" : "22/8/1995", "Hieght" : "5'9" , "City" : "Jaipur" , "State" : "Odisha" }}
+        self.emp_list=[self.e1,self.e2,self.e3,self.e4,self.e5,self.e6,self.e7,self.e8]
+        for i in self.emp_list:
+            self.Employeesdata.update(i)
+        with open ("employee.json","w") as e:
+            self.x=json.dump(self.Employeesdata,e)
+    def read_Emp_data(self):
+        with open("employee.json","r") as e_read:
+            self.Employee=json.load(e_read)
+        
+            for i, j in self.Employee.items():
+                print (i,"=",j)    
+e= Employee()
+e.read_Emp_data()
 
 # problem 2
 import json
